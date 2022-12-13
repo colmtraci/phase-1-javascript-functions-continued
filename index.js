abc7c73359cbfe003f4f9875a5b783401aad94d8
Colm Traci <ctraci@me.com>
2022-12-13 10:38:13 -0800

    commit tree d3eecaaf66abf3cc8d30da2e44f82943eeab6e1c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun (default1 = 'roller-skate'){
+    return(`This Saturday, I want to ${default1}!`);
+}
+saturdayFun();
+
+function mondayWork (default2 = 'go to the office'){
+    return (`This Monday, I will ${default2}.`);
+}
+mondayWork();
+
+
+function wrapAdjective() {
+    return function (default3 = 'special') {
+        return (`You are ${default3}!`)
+       // const part2 = "there";
+        //return function () {
+            //return(`${part1.toUpperCase()} ${part2} ${name}`);
+        //};
+    };
+}
+  
+demoChain("Dr. Stephen Strange")()(); //=> HI there Dr. Stephen Strange
